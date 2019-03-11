443c1630ee057f86ab889823c9ee409055e916be
sagartripathi <sagartripathi@172.31.95.144>
2019-03-11 14:40:01 +0800

    BddFrameWork Automation Test Project Started

---
+++ ProjectBdd/src/main/java/BddFramework/ProjectBdd/App.java
@@ -0,0 +1,13 @@
+package BddFramework.ProjectBdd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
